abb7e8006f6c2c57dcc9e1e80d3e220c9cc9cea3
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2024-09-23 11:30:55 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder();
-        //System.out.println(f.currentFood);
         Feeder g = new Feeder(500);
         int random = (int)(Math.random() * 50 + 1);
         Feeder i = new Feeder(100);
         i.simulateOneDay(5);
         System.out.println(i);
+        Feeder j = new Feeder(0);
+        System.out.println(j.simulateManyDays(5, 10));
 
     }
 
