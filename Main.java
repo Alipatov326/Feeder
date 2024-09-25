3f9aca40a3b396053b77e967c798a3c6899d05f7
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2024-09-25 11:35:41 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,28 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder g = new Feeder(500);
-        int random = (int)(Math.random() * 50 + 1);
-        Feeder i = new Feeder(100);
-        i.simulateOneDay(5);
-        System.out.println(i);
-        Feeder j = new Feeder(0);
-        System.out.println(j.simulateManyDays(5, 10));
+        //Test 1
+        Feeder a = new Feeder(2400);
+        System.out.println(a.simulateManyDays(10, 4));
+        //Test 2
+        Feeder b = new Feeder(250);
+        System.out.println(b.simulateManyDays(10, 5));
+        //Test 3
+        Feeder c = new Feeder(0);
+        System.out.println(c.simulateManyDays(5, 10));
+        //Test 4
+        Feeder d = new Feeder(500);
+        d.simulateOneDay(12);
+        System.out.println(d);
+        //Test 5
+        Feeder e = new Feeder(1000);
+        e.simulateOneDay(22);
+        System.out.println(e);
+        //Test 6
+        Feeder f = new Feeder(100);
+        f.simulateOneDay(5);
+        System.out.println(f);
+
 
     }
 
