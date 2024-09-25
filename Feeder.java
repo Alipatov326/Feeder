33eb9f79172d4ebb8d9c1e284900789717287337
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2024-09-25 11:47:36 -0400

    Update Feeder.java

--- Feeder.java
+++ Feeder.java
@@ -46,10 +46,10 @@ public class Feeder {
         int days = 0;
         while(numDays > 0)
         {
+            if(currentFood == 0) return days;
             simulateOneDay(numBirds);
             days++;
             numDays--;
-            if(currentFood == 0) return days;
         }
 
         return days;
